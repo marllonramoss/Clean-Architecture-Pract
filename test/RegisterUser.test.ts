e5f9f89481cf37c0a6f6aae1dac445e32cc2bf25
Marllon Ramos <marllonsteam@gmail.com>
2024-05-17 17:28:43 -0300

    Update RegisterUser.test.ts

--- test/RegisterUser.test.ts
+++ test/RegisterUser.test.ts
@@ -3,20 +3,36 @@ import DbOnMemory from "../src/example/adapters/db/UserOnMemory"
 import RegisterUser from "../src/example/core/user/RegisterUser"
 import UserRegister from "../src/example/core/user/UserRegister"
 
-test('Want to insert a user on DbOnMemory with crypted password', () => {
+test('Want to insert a user on DbOnMemory with encrypted password', async () => {
     const db = new DbOnMemory()
     const bcrypt = new Cryptography()
     const useCase = new UserRegister(db, bcrypt)
 
-    const user = useCase.execute('Robert', 'robert@zmail.com', '123456')
+    const user = await useCase.execute('Robert', 'robert@zmail.com', '123456')
 
     expect(user).toHaveProperty('id')
     expect(user.name).toBe('Robert')
     expect(user.email).toBe('robert@zmail.com')
     
-    console.log(user.password);
+    console.log(`user id: ${user.id}`);
+    console.log(`user password: ${user.password}`);
     expect(bcrypt.compare('123456', user.password!)).toBeTruthy()
     
+    
+})
+test('It should throw an error when attempting to register a user that is already ', async () => {
+    const db = new DbOnMemory()
+    const bcrypt = new Cryptography()
+    const useCase = new UserRegister(db, bcrypt)
+
+    const name = 'Joao da Silva'
+    const email = 'jjoao@zmail.com'
+    const password = '123456'
+
+    await useCase.execute(name, email, password)
+    const exec = async () => await useCase.execute(name, email, password)
+
+    await expect(exec).rejects.toThrowError('User already exists')
     
 })
 
